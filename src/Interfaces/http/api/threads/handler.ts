7382d5d87f1c3ea8206519bf8779ac181ac23a6c
Dias Utsman <utsmand91@gmail.com>
2023-05-10 13:06:04 +0700

    remove unnecessary functions

--- src/Interfaces/http/api/threads/handler.ts
+++ src/Interfaces/http/api/threads/handler.ts
@@ -5,10 +5,8 @@
  * @typedef {import('@hapi/hapi').ResponseObject} ResponseObject
  */
 import AddThreadUseCase from '../../../../Applications/use_case/ThreadUseCase';
-import CommentUseCase from '../../../../Applications/use_case/CommentUseCase';
 import ThreadUseCase from '../../../../Applications/use_case/ThreadUseCase';
 import autoBind from 'auto-bind';
-import ReplyUseCase from '../../../../Applications/use_case/ReplyUseCase';
 import { Container } from 'src/Infrastructures/container';
 import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi';
 
@@ -50,31 +48,6 @@ class ThreadsHandler {
 
   /**
    * @param {Request} request
-   * @param {ResponseToolkit} h
-   * @return {ResponseObject}
-   * @memberof ThreadsHandler
-   */
-  async postCommentHandler(request: Request, h: ResponseToolkit): Promise<ResponseObject> {
-    /** @type {CommentUseCase} */
-    const commentUseCase = this._container.getInstance(CommentUseCase.name);
-    const addedComment = await commentUseCase.addComment({
-      ...request.payload as object,
-      owner: request.auth.credentials.id,
-      threadId: request.params.threadId,
-    });
-
-    const response = h.response({
-      status: 'success',
-      data: {
-        addedComment,
-      },
-    });
-    response.code(201);
-    return response;
-  }
-
-  /**
-   * @param {Request} request
    * @return {ResponseObject}
    * @memberof ThreadsHandler
    */
@@ -89,82 +62,6 @@ class ThreadsHandler {
       data: {
         thread,
       },
-    };
-  }
-
-  /**
-   * @param {Request} request
-   * @return {ResponseObject}
-   * @memberof ThreadsHandler
-   */
-  async deleteThreadCommentByIdHandler(request: Request): Promise<ResponseObject | object> {
-    const {threadId, commentId} = request.params;
-    const {id: credentialId} = request.auth.credentials;
-
-    /** @type {CommentUseCase} */
-    const commentUseCase = this._container.getInstance(CommentUseCase.name);
-    await commentUseCase.deleteComment({
-      commentId,
-      threadId,
-      owner: credentialId,
-    });
-
-    return {
-      status: 'success',
-      message: 'berhasil menghapus komentar',
-    };
-  }
-
-  /**
-   * @param {Request} request
-   * @param {ResponseToolkit} h
-   * @return {ResponseObject}
-   * @memberof ThreadsHandler
-   */
-  async postReplyHandler(request: Request, h: ResponseToolkit): Promise<ResponseObject> {
-    const {threadId, commentId} = request.params;
-    const {id: credentialId} = request.auth.credentials;
-
-    /** @type {ReplyUseCase} */
-    const replyUseCase = this._container.getInstance(ReplyUseCase.name);
-    const addedReply = await replyUseCase.addReply({
-      ...request.payload as object,
-      owner: credentialId,
-      threadId,
-      commentId,
-    });
-
-    const response = h.response({
-      status: 'success',
-      data: {
-        addedReply,
-      },
-    });
-    response.code(201);
-    return response;
-  }
-
-  /**
-   * @param {Request} request
-   * @return {ResponseObject}
-   * @memberof ThreadsHandler
-   */
-  async deleteReplyHandler(request: Request): Promise<ResponseObject | object> {
-    const {threadId, commentId, replyId} = request.params;
-    const {id: credentialId} = request.auth.credentials;
-
-    /** @type {ReplyUseCase} */
-    const replyUseCase = this._container.getInstance(ReplyUseCase.name);
-    await replyUseCase.deleteReply({
-      threadId,
-      commentId,
-      replyId,
-      owner: credentialId,
-    });
-
-    return {
-      status: 'success',
-      message: 'berhasil menghapus balasan',
     };
   }
 }
